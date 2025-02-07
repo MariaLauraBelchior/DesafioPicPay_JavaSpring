7ee123cf737b5de70d25fd648a8e10d95149aeff
MariaLauraBelchior <marialaurabelchior007@gmail.com>
2025-02-06 23:24:09 -0300

    first commit

---
+++ src/main/java/com/desafio/picpaysimp/PicpaysimpApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.picpaysimp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaysimpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaysimpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/picpaysimp/PicpaysimpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.picpaysimp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaysimpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
